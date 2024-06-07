f2509cb579e99a92276106d73bc9dd8dc002ee63
adminlogs <adel.box@outlook.com>
2024-06-07 16:30:14 +0100

    Initial configuration

---
+++ noip.php
@@ -0,0 +1,14 @@
+<?php
+$user = '';
+$pass = '';
+$host = '';
+
+$r_value = file_get_contents('http://checkip.dyndns.com/');
+preg_match('/Current IP Address: ([.0-9]+)/', $r_value, $ips);
+$ip = $ips[1];
+
+$payload = 'username='.$user.'&pass='.$pass.'&h[]='.$host.'&ip='.$ip;
+$payload = base64_encode($payload);
+
+file_get_contents('https://dynupdate.no-ip.com/ducupdate.php?requestL='.$payload);
+?>
