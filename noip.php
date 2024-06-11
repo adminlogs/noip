e7cb4d22c552e5d372dfc13fcd264fd2d90ac460
adminlogs <adel.box@outlook.com>
2024-06-11 02:52:07 +0100

    encode  variable

--- noip.php
+++ noip.php
@@ -7,7 +7,7 @@ $r_value = file_get_contents('http://checkip.dyndns.com/');
 preg_match('/Current IP Address: ([.0-9]+)/', $r_value, $ips);
 $ip = $ips[1];
 
-$payload = 'username='.$user.'&pass='.$pass.'&h[]='.$host.'&ip='.$ip;
+$payload = 'username='.urlencode($user).'&pass='.urlencode($pass).'&h[]='.$host.'&ip='.$ip;
 $payload = base64_encode($payload);
 
 file_get_contents('https://dynupdate.no-ip.com/ducupdate.php?requestL='.$payload);
